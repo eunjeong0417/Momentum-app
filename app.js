b8c6a417ca0a0106c59b693bb44369da89542902
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-12 19:30:19 +0900

    version4

--- app.js
+++ app.js
@@ -9,11 +9,8 @@ const loginButton = loginForm.querySelector("button");
 function handleClick (event) {
     event.preventDefault();
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    console.log(username);
-    greeting.innerText = "hello " + username;
-    greeting.classList.remove("hidden");
     localStorage.setItem("username", username);
+    paintGreeting (username);
 }
 /*localstorage에 저장될 값의 이름 (key)을 정해주고 그 다음에는 값인 username을 적어준다 */
 
@@ -24,3 +21,19 @@ loginForm.addEventListener("submit", handleClick );
 /*form태그에 이벤트를 걸어준다.
 엔터를 누르거나 버튼이 클릭되면 handleClick이 실행된다.
 기본적으로 실행되는 이벤트를 막고 싶다면 preventDefault를 넣어주면 된다*/
+
+function paintGreeting (username) {
+    greeting.innerText = "hello " + username;
+    greeting.classList.remove("hidden");
+    
+}
+
+const savedUsername = localStorage.getItem("username");
+
+
+if (savedUsername === null) {
+    loginForm.classList.remove("hidden");
+    loginForm.addEventListener("submit", handleClick );
+} else {
+    paintGreeting(savedUsername);
+}
