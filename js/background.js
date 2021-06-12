bb03865a89ad2278ccd790b358eee7c792ad65e5
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-13 00:52:02 +0900

    version8 random image 추가

---
+++ js/background.js
@@ -0,0 +1,18 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+]
+/*이미지 배열을 만들때는 파일명과 동일하게 해주면 된다. */
+
+
+
+const randomImg = (images[Math.floor(Math.random() * images.length)]);
+
+/*자바스크립트에서 생성해서 html에 추가해준다 */
+
+const chosenImg = document.createElement("img");
+
+chosenImg.src = `img/${randomImg}`;
+
+document.body.appendChild(chosenImg)
