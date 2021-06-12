351b3747c5e7530a91363b9355b9d27ca5e28500
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-12 20:25:19 +0900

    version5 setInterval 기능 추가

---
+++ js/clock.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("h2#clock");
+
+function hello () {
+    console.log("hello, nice to meet you");
+}
+
+setInterval(hello , 5000);
+setInterval
--- js/greetings.js
+++ js/greetings.js
@@ -37,3 +37,4 @@ if (savedUsername === null) {
 } else {
     paintGreeting(savedUsername);
 }
+
